6955136fb27a1afc41613119507ad6b7aa421ebc
Dmytro Bulhakov <mail@dbulgakov.com>
2023-11-17 17:14:56 +0200

    added music

--- lab6/app/src/main/java/com/example/lab6/OpenGLES20Activity.java
+++ lab6/app/src/main/java/com/example/lab6/OpenGLES20Activity.java
@@ -3,6 +3,7 @@ package com.example.lab6;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
@@ -11,24 +12,41 @@ import android.util.Log;
 public class OpenGLES20Activity extends Activity {
     static final String TAG = "OpenGLES20Activity";
     GLSurfaceView mGLView;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mGLView = new MyGLSurfaceView(this);
         setContentView(mGLView);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mGLView.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mGLView.onResume();
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
     public static int loadShader(int type, String shaderCode) {
